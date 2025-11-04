a09b7aaeab7285bbcb465ee966925057d7e47817
antonythomas123 <antonythomas993@gmail.com>
2025-11-04 22:49:43 +0530

    Initial commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumey.io" },
+    { name: "description", content: "Unlock your career potential!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main></main>;
 }
