eaaf7abd9e648678aaae2e516fc78be5bef19b32
antonythomas123 <antonythomas993@gmail.com>
2025-11-06 08:36:02 +0530

    Resume feedback section

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,6 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function formatSize(bytes: number): string {
   if (bytes === 0) return "0 Bytes";
 
@@ -9,5 +12,8 @@ export function formatSize(bytes: number): string {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
 }
 
+export const generateUUID = () => crypto.randomUUID();
 
-export const generateUUID = () => crypto.randomUUID()
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
