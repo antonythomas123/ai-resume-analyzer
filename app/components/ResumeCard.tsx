8a704ce235dea6138e78f9ae541e2ca3b3a1acea
antonythomas123 <antonythomas993@gmail.com>
2025-11-07 11:47:36 +0530

    Reusme on homepage from puterstore

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
 import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({ resume }: { resume: Resume }) => {
+  const { fs } = usePuterStore();
+
+  const [resumeUrl, setResumeUrl] = useState("");
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(resume.imagePath);
+
+      if (!blob) return;
+
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+    loadResume();
+  }, [resume.imagePath]);
+
   return (
     <Link
       to={`/resume/${resume.id}`}
@@ -10,28 +27,37 @@ const ResumeCard = ({ resume }: { resume: Resume }) => {
     >
       <div className="resume-card-header">
         <div className="flex flex-col gap-2">
-          <h2 className="text-black! font-bold wrap-break-word">
-            {resume.companyName}
-          </h2>
-          <h3 className="text-lg wrap-break-word text-gray-500">
-            {resume.jobTitle}
-          </h3>
+          {resume?.companyName && (
+            <h2 className="text-black! font-bold wrap-break-word">
+              {resume.companyName}
+            </h2>
+          )}
+          {resume.jobTitle && (
+            <h3 className="text-lg wrap-break-word text-gray-500">
+              {resume.jobTitle}
+            </h3>
+          )}
+          {!resume.companyName && !resume?.jobTitle && (
+            <h2 className="text-black! font-bold">Resume</h2>
+          )}
         </div>
 
-        <div className="flex-shrink-0">
+        <div className="shrink-0">
           <ScoreCircle score={resume.feedback.overallScore} />
         </div>
       </div>
 
-      <div className="gradient-border animate-in fade-in duration-1000">
-        <div className="w-full h-full">
-          <img
-            src={resume.imagePath}
-            alt="resume"
-            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-          />
+      {resumeUrl && (
+        <div className="gradient-border animate-in fade-in duration-1000">
+          <div className="w-full h-full">
+            <img
+              src={resumeUrl}
+              alt="resume"
+              className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+            />
+          </div>
         </div>
-      </div>
+      )}
     </Link>
   );
 };
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "constants/index";
 import ResumeCard from "~/components/ResumeCard";
 import { usePuterStore } from "~/lib/puter";
-import { useNavigate } from "react-router";
-import { useEffect } from "react";
+import { Link, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -14,13 +13,32 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const { auth } = usePuterStore();
+  const { auth, kv } = usePuterStore();
 
   const navigate = useNavigate();
+
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!auth.isAuthenticated) navigate("/auth?next=/");
   }, [auth.isAuthenticated]);
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoading(true);
+
+      const res = (await kv.list("resume:*", true)) as KVItem[];
+
+      const parsedResumes = res.map(
+        (resume) => JSON.parse(resume.value) as Resume
+      );
+
+      setResumes(parsedResumes || []);
+      setLoading(false);
+    };
+    loadResumes();
+  }, []);
 
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -29,14 +47,36 @@ export default function Home() {
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
-          <h2>Review your submissions and check AI-powered feedback</h2>
+
+          {!loading && resumes.length === 0 ? (
+            <h2>No Resumes found. Upload your first resume to get feedback</h2>
+          ) : (
+            <h2>Review your submissions and check AI-powered feedback</h2>
+          )}
         </div>
 
-        {resumes?.length > 0 && (
+        {loading && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px] " />
+          </div>
+        )}
+
+        {!loading && resumes?.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume: Resume) => {
               return <ResumeCard resume={resume} />;
             })}
+          </div>
+        )}
+
+        {!loading && resumes.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link
+              to={"/upload"}
+              className="primary-button w-fit text-xl font-semibold"
+            >
+              Upload Resume
+            </Link>
           </div>
         )}
       </section>
